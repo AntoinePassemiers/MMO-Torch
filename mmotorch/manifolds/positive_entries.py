0f7b2db6c234cec157e8001389183720b481bc21
AntoinePassemiers <antoine.passemiers@gmail.com>
2021-01-26 14:20:16 +0100

    PositiveEntriesManifold: numerically stable retraction

--- mmotorch/manifolds/positive_entries.py
+++ mmotorch/manifolds/positive_entries.py
@@ -6,28 +6,35 @@ from mmotorch.manifolds.base import Manifold
 
 import numpy as np
 import torch
+import warnings
 
 
 class PositiveEntriesManifold(Manifold):
 
-    def __init__(self, n, m, epsilon=1e-8, **kwargs):
-        Manifold.__init__(self, n, m, **kwargs)
+    def __init__(self, *shape, epsilon=1e-8, **kwargs):
+        Manifold.__init__(self, *shape, **kwargs)
         self.epsilon = epsilon
 
     def _init(self):
-        return np.random.rand(self.n, self.m)
+        return np.exp(np.random.rand(self.n, self.m))
 
     def _egrad_to_rgrad(self, X, G):
-        return X * X * G
+        return X * G * X
 
     def _retraction(self, X, G):
         X = np.maximum(X, self.epsilon)
         Y = np.log(X) + (G / X)
         Y = np.exp(Y)
-        Y = np.maximum(Y, self.epsilon)
-        if np.any(np.isnan(Y)):
+        if np.any(np.isnan(Y)) or np.any(np.isinf(Y)):
             warnings.warn('Invalid value encountered during retraction mapping onto PositiveEntriesManifold')
-        return np.nan_to_num(Y)
+            mask = np.isinf(Y)
+            Y[mask] = X[mask]
+            Y = np.nan_to_num(Y)
+        Y = np.maximum(Y, self.epsilon)
+
+        # Weighted geometric mean for smoothing
+        Y = np.exp(0.1 * np.log(Y) + 0.9 * np.log(X))  # TODO
+        return Y
 
     def _inner(self, X, G, H):
         return torch.sum((G * H) / (X * X))
